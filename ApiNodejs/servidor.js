b994b71173e5d58cdbc25ac1c27c41ee0fa24c46
jorgec815 <jorgecortes815@gmail.com>
2021-09-26 19:35:07 -0500

    Barbas yo te elijo

---
+++ ApiNodejs/servidor.js
@@ -0,0 +1,177 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var bodyparser = require('body-parser');
+var oracledb = require('oracledb');
+var consecutivo = 100;
+//Authoriser tous les requettes cors)
+var cors = require('cors');
+app.use(cors());
+
+app.use(bodyparser.json());
+
+///Pour changer le format de la requete 
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+
+
+var connAttrs = {
+    "user": "system",
+    "password": "0815",
+    "connectString": "(DESCRIPTION =(LOAD_BALANCE = ON)(FAILOVER = ON)(ADDRESS =(PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))(ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)(FAILOVER_MODE=(TYPE=SELECT)(METHOD = BASIC))))"
+}
+
+/////Consultar usuarios
+app.get('/clientes', function (req, res) {
+    "use strict";
+
+    oracledb.getConnection(connAttrs, function (err, connection) {
+        if (err) {
+            // Error connecting to DB
+            res.set('Content-Type', 'application/json');
+            res.status(500).send(JSON.stringify({
+                status: 500,
+                message: "Error connecting to DB",
+                detailed_message: err.message
+            }));
+            return;
+        }
+        connection.execute("SELECT * FROM cliente", {}, {
+            outFormat: oracledb.OBJECT // Return the result as Object
+        }, function (err, result) {
+            if (err) {
+                res.set('Content-Type', 'application/json');
+                res.status(500).send(JSON.stringify({
+                    status: 500,
+                    message: "Error getting the dba_tablespaces",
+                    detailed_message: err.message
+                }));
+            } else {
+                res.header('Access-Control-Allow-Origin', '*');
+                res.header('Access-Control-Allow-Headers', 'Content-Type');
+                res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+                res.contentType('application/json').status(200);
+                res.send(JSON.stringify(result.rows));
+
+            }
+            // Release the connection
+            connection.release(
+                function (err) {
+                    if (err) {
+                        console.error(err.message);
+                    } else {
+                        console.log("GET /sendTablespace : Connection released");
+                    }
+                });
+        });
+    });
+});
+
+/////Supprimer un Utilisateur (node.js)////// done
+app.delete('/eliminar_cliente/:id', function (req, res, next) {
+
+    var id = req.params.id;
+    oracledb.getConnection(connAttrs, function (err, connection) {
+        if (err) {
+            // Error connecting to DB
+            res.set('Content-Type', 'application/json');
+            res.status(500).send(JSON.stringify({
+                status: 500,
+                message: "Error connecting to DB",
+                detailed_message: err.message
+            }));
+            return;
+        }
+        connection.execute("DELETE FROM cliente WHERE id=" + id, {}, {
+            outFormat: oracledb.OBJECT // Return the result as Object
+        }, function (err, result) {
+            if (err) {
+                res.header('Access-Control-Allow-Origin', '*');
+                res.header('Access-Control-Allow-Headers', 'Content-Type');
+                res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+                res.contentType('application/json').status(200);
+                res.send(JSON.stringify(err.message));
+
+            } else {
+                res.header('Access-Control-Allow-Origin', '*');
+                res.header('Access-Control-Allow-Headers', 'Content-Type');
+                res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+                res.contentType('application/json').status(200);
+                res.send(JSON.stringify("1"));
+
+            }
+            // Release the connection
+            connection.release(
+                function (err) {
+                    if (err) {
+                        console.error(err.message);
+                    } else {
+                        console.log("POST /sendTablespace : Connection released");
+                    }
+                });
+        });
+    });
+});
+
+app.post('/cliente', function (req,res,next) {
+    var id = consecutivo
+    var nombre = new String(req.body.nombre)
+    var apellido = new String(req.body.apellido)
+    var ano = req.body.ano
+    var mes = req.body.mes
+    var dia = req.body.dia
+    var fechaNacimiento = new Date(Date.UTC(ano, mes, dia, 0, 0, 0));
+    
+    var correo = req.body.correo
+    var usuario = nombre.substr(0,2) + apellido.substr(-3,3) + consecutivo 
+    consecutivo = consecutivo +1
+
+
+      oracledb.getConnection(connAttrs, function (err, connection) {
+        if (err) {
+            // Error connecting to DB
+            res.set('Content-Type', 'application/json');
+            res.status(500).send(JSON.stringify({
+                status: 500,
+                message: "Error connecting to DB",
+                detailed_message: err.message
+            }));
+            return;
+        }
+
+        connection.execute("INSERT INTO \"SYSTEM\".\"CLIENTE\" (ID, NOMBRE, APELLIDO, FECHA_NACIMIENTO, CORREO, USUARIO) VALUES ("+id+",'"+nombre+"','"+apellido+"',TO_DATE('1999-09-16 17:23:44', 'YYYY-MM-DD HH24:MI:SS'),'"+correo+"','"+usuario+"')", {autoCommit: true}, {
+            outFormat: oracledb.OBJECT // Return the result as Object
+        }, function (err, result) {
+            if (err) {
+                res.header('Access-Control-Allow-Origin','*'); 
+                res.header('Access-Control-Allow-Headers','Content-Type');
+                res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,OPTIONS');
+                res.contentType('application/json').status(200);
+                res.send(JSON.stringify(err.message)) ;
+               
+            } else {
+                res.header('Access-Control-Allow-Origin','*');
+                res.header('Access-Control-Allow-Headers','Content-Type');
+                res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,OPTIONS');
+                res.contentType('application/json').status(200);
+                res.send(JSON.stringify("1"))   ; 
+                
+            }
+            // Release the connection
+            connection.release(
+                function (err) {
+                    if (err) {
+                        console.error(err.message);
+                    } else {
+                        console.log("POST /sendTablespace : Connection released");
+                    }
+                });
+        });
+    });
+   
+});
+
+app.listen(3000, function () {
+    console.log("Live at Port 3000");
+});
